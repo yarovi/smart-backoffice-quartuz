1083ef568262de147d18e03e10ce0610a0507df2
yasmani <tas185@gmail.com>
2025-06-21 08:49:31 -0500

    add configuration to auth form with login

--- src/main/java/org/inamsay/net/tables/TablesResource.java
+++ src/main/java/org/inamsay/net/tables/TablesResource.java
@@ -2,7 +2,9 @@ package org.inamsay.net.tables;
 
 
 import io.quarkus.hibernate.reactive.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.ResourceProperties;
 
+@ResourceProperties(rolesAllowed={"user"})
 public interface TablesResource extends PanacheEntityResource< Table, Long> {
 
 
